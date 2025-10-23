6fdcd389da513ae3a5e3e26d72309109cd2a058c
ChiharuYanai-tcs <chiharu.yanai@tcs.com>
2025-10-23 13:47:20 +0000

    Added gitignore

--- CSharp100Knocks/obj/Debug/net8.0/CSharp100Knocks.AssemblyInfo.cs
+++ CSharp100Knocks/obj/Debug/net8.0/CSharp100Knocks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp100Knocks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94da70b5ba30f4c524557f6c13cf708a600a39ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5110c8790445bfbf33c37dfdd9a752cac399b5d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp100Knocks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp100Knocks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
